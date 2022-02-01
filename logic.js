f36644a0bd5ba4ec07e377fd3ddb19c3c996ffe2
Jonah <jonah.watts@gmail.com>
2022-01-31 22:29:05 -0700

    In class notes

---
+++ logic.js
@@ -0,0 +1,16 @@
+document.write("<table>");
+for (var i = 0; i < 10; i++)
+{
+    document.write("<tr>");
+
+    for (var j = 0; j < 10; j++)
+    {
+        if (i == j)
+            document.write("<td class = \'diag\'>(" + i + "," + j + ")</td>");
+        else
+            document.write("<td>(" + i + "," + j + ")</td>");
+    }
+
+    document.write("</tr>");
+}
+document.write("</table>");
