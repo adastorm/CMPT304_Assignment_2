0c764523f9d9b4f016f48cbacc9c2ce216e555de
Jonah <jonah.watts@gmail.com>
2022-02-01 09:31:21 -0700

    Comment out the lecutre notes js

--- logic.js
+++ logic.js
@@ -1,16 +1,16 @@
-document.write("<table>");
-for (var i = 0; i < 10; i++)
-{
-    document.write("<tr>");
+// document.write("<table>");
+// for (var i = 0; i < 10; i++)
+// {
+//     document.write("<tr>");
 
-    for (var j = 0; j < 10; j++)
-    {
-        if (i == j)
-            document.write("<td class = \'diag\'>(" + i + "," + j + ")</td>");
-        else
-            document.write("<td>(" + i + "," + j + ")</td>");
-    }
+//     for (var j = 0; j < 10; j++)
+//     {
+//         if (i == j)
+//             document.write("<td class = \'diag\'>(" + i + "," + j + ")</td>");
+//         else
+//             document.write("<td>(" + i + "," + j + ")</td>");
+//     }
 
-    document.write("</tr>");
-}
-document.write("</table>");
+//     document.write("</tr>");
+// }
+// document.write("</table>");
